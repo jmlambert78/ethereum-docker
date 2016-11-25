c80d95bca61dac1e07099366eeed0ecddcd99258
jmlambert78 <jmlambert78@gmail.com>
2016-11-25 20:07:16 +0100

    Keep ipc path as initially

--- monitored-geth-client/start.sh
+++ monitored-geth-client/start.sh
@@ -5,7 +5,7 @@ perl -pi -e "s/XXX/$(hostname)/g" app.json
 /usr/bin/pm2 start ./app.json
 sleep 3
 mkdir ~/.ethereum/devchain/$(hostname)
-/usr/bin/geth --datadir=~/.ethereum/devchain/$(hostname) init "/root/files/genesis.json"
+/usr/bin/geth --datadir=~/.ethereum/devchain/$(hostname) --ipcpath "~/.ethereum/devchain/geth.ipc"  init "/root/files/genesis.json"
 sleep 3
 BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
 GETH_OPTS=${@/XXX/$BOOTSTRAP_IP}
