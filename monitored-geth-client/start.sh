e9487946af13b6238a7d8d19dabb314d4bf562bd
jmlambert78 <jmlambert78@gmail.com>
2016-11-25 19:49:55 +0100

    change area of storage per container

--- monitored-geth-client/start.sh
+++ monitored-geth-client/start.sh
@@ -4,8 +4,10 @@ cd /root/eth-net-intelligence-api
 perl -pi -e "s/XXX/$(hostname)/g" app.json
 /usr/bin/pm2 start ./app.json
 sleep 3
-/usr/bin/geth --datadir=~/.ethereum/devchain init "/root/files/genesis.json"
+/usr/bin/geth --datadir=~/.ethereum/devchain/$hostname init "/root/files/genesis.json"
 sleep 3
 BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
 GETH_OPTS=${@/XXX/$BOOTSTRAP_IP}
+GETH_OPTS=`echo "$GETH_OPTS"|sed s/YYYHOSTNAME/$(hostname)/`
+echo "GETH_OPTS=$GETH_OPTS"
 /usr/bin/geth $GETH_OPTS
