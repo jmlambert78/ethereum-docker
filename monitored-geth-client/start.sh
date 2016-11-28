e5bd5ae48823ba4c3271851c3408bbf6e1b27789
jmlambert78 <jmlambert78@gmail.com>
2016-11-28 11:24:03 +0100

    update ipc

--- monitored-geth-client/start.sh
+++ monitored-geth-client/start.sh
@@ -4,7 +4,13 @@ cd /root/eth-net-intelligence-api
 perl -pi -e "s/XXX/$(hostname)/g" app.json
 /usr/bin/pm2 start ./app.json
 sleep 3
-mkdir ~/.ethereum/devchain/$(hostname)
+if [ -d "~/.ethereum/devchain/$(hostname)" ]; then
+ echo "Area exists"
+else
+ echo "existe pas"
+ mkdir ~/.ethereum/devchain/$(hostname)
+
+fi
 /usr/bin/geth --datadir=~/.ethereum/devchain/$(hostname) --ipcpath "~/.ethereum/devchain/geth.ipc"  init "/root/files/genesis.json"
 sleep 3
 BOOTSTRAP_IP=`getent hosts bootstrap | cut -d" " -f1`
